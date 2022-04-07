97f34b9d132ac1deb6aee831287a6ed8fb7baf6d
EC2 Default User <ec2-user@ip-172-31-87-70.ec2.internal>
2022-04-07 11:36:46 +0000

    [Add]

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -4,9 +4,9 @@ class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
   # def create
@@ -24,4 +24,7 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def after_sign_in_path_for(resource)
+    admin_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,22 @@ Rails.application.routes.draw do
    sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#top"
-  resources :admin, only: [:new,]
-  resources :public, only: []
+
+  namespace :admin do
+   get '/' => 'homes#top'
+   resources :items
+   resources :genres, only: [:index, :create,  :edit, :update]
+   resources :customers, only: [:index, :show,  :edit, :update]
+   resources :oders, only: [:show, :update]
+   resources :oder_details, only: [:update]
+  end
+  scope module: :public do
+   root to: "homes#top"
+   get 'home/about' => 'homes#about', as: 'about'
+   resources :items, only: [:index, :show]
+   resources :customers, only: [:show, :edit, :update,]
+   resources :cart_items, only: [:index, :update, :destroy, :create]
+   resources :orders, only: [:new, :index, :create, :cr]
+   resources :addresses, only: [:index, :show, :create, :update, :destroy]
+  end
 end

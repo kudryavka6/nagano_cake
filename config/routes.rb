a4c6dd1d303de571aa64056f7195a2bb5f2e693c
EC2 Default User <ec2-user@ip-172-31-87-70.ec2.internal>
2022-04-07 00:33:10 +0000

    [Add]開発者レイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :customers,skip: [:passwords], controllers: {
+   registrations: "public/registrations",
+   sessions: 'public/sessions'
+  }
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+   sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  resources :admin, only: [:new,]
+  resources :public, only: []
 end

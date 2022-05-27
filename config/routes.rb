b21097e3709cf92ecd9adace68f6e30ec46f0dbf
EC2 Default User <ec2-user@ip-172-31-87-70.ec2.internal>
2022-05-27 11:11:11 +0000

    [Add]登録情報編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  scope module: :public do
+   root to: "homes#top"
+   get 'home/about' => 'homes#about', as: 'about'
+   get 'customers/my_page' => 'customers#show'
+   get 'customers/edit' => 'customers#edit'
+   resources :items, only: [:index, :show]
+   resources :customers, only: [:update,]
+   resources :cart_items, only: [:index, :update, :destroy, :create]
+   resources :orders, only: [:new, :index, :create, :cr]
+   resources :addresses, only: [:index, :show, :create, :update, :destroy]
+  end
   devise_for :customers,skip: [:passwords], controllers: {
    registrations: "public/registrations",
    sessions: 'public/sessions'
@@ -15,14 +26,5 @@ Rails.application.routes.draw do
    resources :customers, only: [:index, :show,  :edit, :update]
    resources :orders, only: [:show, :update]
    resources :order_details, only: [:update]
-  end
-  scope module: :public do
-   root to: "homes#top"
-   get 'home/about' => 'homes#about', as: 'about'
-   resources :items, only: [:index, :show]
-   resources :customers, only: [:show, :edit, :update,]
-   resources :cart_items, only: [:index, :update, :destroy, :create]
-   resources :orders, only: [:new, :index, :create, :cr]
-   resources :addresses, only: [:index, :show, :create, :update, :destroy]
   end
 end

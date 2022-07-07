7e8d1f70227b1e2a196791e695e0a2b9a683458f
EC2 Default User <ec2-user@ip-172-31-87-70.ec2.internal>
2022-07-07 11:09:00 +0000

    [start]注文情報入力

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,18 +1,23 @@
 class Public::OrdersController < ApplicationController
   def new
-    
+    @order = Order.new
   end
-  
+
   def index
-    
+
   end
-  
+
   def show
-    
+
   end
-  
+
   def create
-    
+
   end
-  
+
+  private
+    def orders
+      params.require(:order).permit(:payment_method, :total_payment)
+    end
+
 end
